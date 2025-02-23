4bfe5c9c21c1e5e5ac2e361daec891c5794491c5
Zoey <1015373877@qq.com>
2025-02-23 12:21:52 +0800

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by 许知知 on 2025/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitSourceControlTests/GitSourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlTests.swift
+//  GitSourceControlTests
+//
+//  Created by 许知知 on 2025/2/23.
+//
+
+import Testing
+@testable import GitSourceControl
+
+struct GitSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
