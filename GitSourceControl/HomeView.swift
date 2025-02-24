4b9faa90e7d31a393adc0330a9a14eeb3e6807c4
xuzhizhi07 <1015373877@qq.com>
2025-02-24 11:39:38 +0800

    HomeView updates [progress]

--- GitSourceControl/HomeView.swift
+++ GitSourceControl/HomeView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, 2!")
+        ZStack {
+            Text("Screen 2")
+            Text("Screen 2")
+            Text("Screen 2")
+        }
     }
 }
 
