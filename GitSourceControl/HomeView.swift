294b658f1173d4884f934161e8d3b8c97527cc39
xuzhizhi07 <1015373877@qq.com>
2025-02-24 11:38:01 +0800

    Add home view

---
+++ GitSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitSourceControl
+//
+//  Created by 抱谷 on 2025/2/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
