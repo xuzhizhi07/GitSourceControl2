63f695517564ac4c5108fcdb33313d6c54d130f1
xuzhizhi07 <1015373877@qq.com>
2025-02-25 10:11:13 +0800

    [feature]delete

--- GitSourceControl/HomeView.swift
+++ GitSourceControl/HomeView.swift
@@ -11,7 +11,7 @@ struct HomeView: View {
     var body: some View {
         ZStack {
             Text("Screen 2")
-            Text("Screen 2")
+            
             Text("Screen 2")
             Text("Screen 2")
         }
