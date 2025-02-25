0cef33725809bd221fdb4c6bbcbb2d4da39a7d54
xuzhizhi07 <1015373877@qq.com>
2025-02-25 10:07:47 +0800

    [feature] update icon and title

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -11,13 +11,14 @@ struct ContentView: View {
     var title:String = ""
     var body: some View {
         VStack {
-            Image(systemName: "heart.fill")
+            Image(systemName: "eraser.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Chris!")
             Text("Hello, zoey!")
             Text("Hello, zoey!")
             Text("Hello, mySecondBranch!")
+            Text("Hello, 2025!")
             
            
         }
